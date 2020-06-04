5a7609c5738b2959bd63fb6d89fe24466dcfbc85
YeongbuCha <dudqn136@naver.com>
2020-06-05 01:09:31 +0900

    Add Input

---
+++ src/components/NeverInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="never-input">
+    <h2>NeverInput</h2>
+      <input
+        v-model="newInput" 
+        @keypress.enter="onInputSubmit"
+        type="text"
+        placeholder="Type and Enter to add neversite"
+        >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NeverInput',
+  data() {
+    return {
+      newInput: ''
+    }
+  },
+  methods: {
+    onInputSubmit() {
+      if (this.newInput.trim()) {
+        this.$emit('inputSubmit', {
+          id: Date.now(),
+          content: this.newInput,
+          isVisited: false
+        })
+      }
+      this.newInput = ''
+    }
+  }
+}
+</script>
+
+<style>
+.never-input {
+  border: 1px solid blue;
+}
+
+input {
+  text-align: center;
+  width: 80%;
+}
+</style>
